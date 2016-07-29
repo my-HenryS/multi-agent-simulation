791bc6bd2a8e6152f3cb2a39bdb79eda5c4d8646
sktjfzt <18910331889@189.cn>
2016-07-30 04:33:49 +0800

    update some document

--- src/org/socialforce/entity/Shape.java
+++ src/org/socialforce/entity/Shape.java
@@ -6,6 +6,18 @@ import java.io.Serializable;
  * Created by Ledenel on 2016/7/28.
  */
 public interface Shape extends Serializable, Cloneable, DimensionEntity{
+    /**
+     * check if a point belongs to a <code>Shape</code>.
+     * @return true if the point is a part of the shape; otherwise false.
+     */
     public boolean contains(Point point);
+    
+    /**
+     * get the distance between two point.
+     * could be multi-dimentional distance
+     * @param point the point that should calculate the distance from
+     * @return distance
+     * @see also dimention
+     */
     public double getDistance(Point point);
 }
