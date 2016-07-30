fdfd3b31c553def4676317c3e5928d679108d418
Ledenel <0xCBA000@gmail.com>
2016-07-30 20:27:35 +0900

    add Force and velocity definitions.

--- src/org/socialforce/entity/Force.java
+++ src/org/socialforce/entity/Force.java
@@ -4,7 +4,6 @@ package org.socialforce.entity;
  * Created by Ledenel on 2016/7/28.
  */
 public interface Force extends Vector {
-    Vector getVelocity(InteractiveEntity from, InteractiveEntity to);
-    Class getSourceClass();
-    Class getTargetClass();
+    Velocity deltaVelocity(double mass, double time);
+
 }
---
+++ src/org/socialforce/entity/Velocity.java
@@ -0,0 +1,8 @@
+package org.socialforce.entity;
+
+/**
+ * Created by Ledenel on 2016/7/30.
+ */
+public interface Velocity extends Vector {
+    Vector deltaDistance(double time);
+}
