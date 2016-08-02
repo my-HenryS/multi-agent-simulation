836ecc23ce8409f147b59589847a1b0856325b18
Ledenel <0xCBA000@gmail.com>
2016-08-02 20:33:23 +0900

    add drawer docs.

--- src/org/socialforce/entity/Drawer.java
+++ src/org/socialforce/entity/Drawer.java
@@ -2,10 +2,37 @@ package org.socialforce.entity;
 
 
 /**
+ * a drawer can draw a specific pattern on a specific device.
+ * the pattern's color can be adjusted.
+ *
+ * @author Ledenel
+ * @see Shape
+ * @see Drawable
  * Created by Ledenel on 2016/7/31.
  */
 public interface Drawer {
+    /**
+     * draw the pattern on the specific device.
+     */
     void draw();
+
+    /**
+     * get the primary color of the pattern. <br/>
+     * the color scheme is ARGB.</br>
+     * CAUTION: there is no guarantee that the color is the color drawed on the device.
+     * please call draw() before to sync the color you get from the device.
+     *
+     * @return
+     */
     int getColor();
+
+    /**
+     * set the primary color of the pattern. <br/>
+     * the color scheme is ARGB.</br>
+     * CAUTION: there is no guarantee that the pattern on the device will changed.
+     * please call draw() to ensure that the changes have been applied on the device.
+     *
+     * @param color
+     */
     void setColor(int color);
 }
