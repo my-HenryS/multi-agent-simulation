1a9388cc2cc30f0714568469bdac8dae3166aa98
Ledenel <0xCBA000@gmail.com>
2016-08-07 20:11:32 +0900

    add junit test support.

---
+++ test/org/socialforce/entity/impl/Vector2DTest.java
@@ -0,0 +1,63 @@
+package org.socialforce.entity.impl;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ledenel on 2016/8/7.
+ */
+public class Vector2DTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void dimension() throws Exception {
+
+    }
+
+    @Test
+    public void add() throws Exception {
+
+    }
+
+    @Test
+    public void equals() throws Exception {
+
+    }
+
+    @Test
+    public void epsilonEquals() throws Exception {
+
+    }
+
+    @Test
+    public void dot() throws Exception {
+
+    }
+
+    @Test
+    public void get() throws Exception {
+
+    }
+
+    @Test
+    public void set() throws Exception {
+
+    }
+
+    @Test
+    public void testClone() throws Exception {
+
+    }
+
+}
