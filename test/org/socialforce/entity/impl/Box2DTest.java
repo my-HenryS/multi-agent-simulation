88c91d5ba083d3337defef64c24de0f19915d932
Ledenel <0xCBA000@gmail.com>
2016-08-09 00:35:16 +0900

    add empty tests.

---
+++ test/org/socialforce/entity/impl/Box2DTest.java
@@ -0,0 +1,66 @@
+package org.socialforce.entity.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ledenel on 2016/8/9.
+ */
+public class Box2DTest {
+    @Test
+    public void contains() throws Exception {
+
+    }
+
+    @Test
+    public void getDistance() throws Exception {
+
+    }
+
+    @Test
+    public void getReferencePoint() throws Exception {
+
+    }
+
+    @Test
+    public void getBounds() throws Exception {
+
+    }
+
+    @Test
+    public void hits() throws Exception {
+
+    }
+
+    @Test
+    public void moveTo() throws Exception {
+
+    }
+
+    @Test
+    public void testClone() throws Exception {
+
+    }
+
+    @Test
+    public void getStartPoint() throws Exception {
+
+    }
+
+    @Test
+    public void getEndPoint() throws Exception {
+
+    }
+
+    @Test
+    public void getSize() throws Exception {
+
+    }
+
+    @Test
+    public void intersect() throws Exception {
+
+    }
+
+}
