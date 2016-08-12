0e6096f63f6ca8036e67c0fae46a04ee1f9c91ce
Ledenel <0xCBA000@gmail.com>
2016-08-12 20:44:40 +0900

    fill in docs of entity.

--- src/org/socialforce/entity/DimensionEntity.java
+++ src/org/socialforce/entity/DimensionEntity.java
@@ -1,6 +1,10 @@
 package org.socialforce.entity;
 
 /**
+ * represent objects which have dimesion-limits.
+ * @author Ledenel
+ * @see Vector
+ * @see Shape
  * Created by Ledenel on 2016/7/28.
  */
 public interface DimensionEntity {
--- src/org/socialforce/entity/InteractiveEntity.java
+++ src/org/socialforce/entity/InteractiveEntity.java
@@ -1,25 +1,55 @@
 package org.socialforce.entity;
 
 /**
- * Created by Ledenel on 2016/7/28.
+ * represent all interactive entities in scene.
+ *
+ * @author Ledenel
+ *         Created by Ledenel on 2016/7/28.
+ * @see Scene
+ * @see Pool
+ * @see Agent
  */
 public interface InteractiveEntity {
-     /**
-     * the prosses of stuff interacting with other stuff
-     * such as agent, wall, fire etc.
-     * @param affectedEntity could be any class of InteractiveEntity
-     * if InteractiveEntity is Agent
-     * @see Shape Force Agent
+    /**
+     * affect the target entity with this.
+     * for example, walls can affect an agent (push them).
+     *
+     * @param affectedEntity the entity to be affected.
+     * @see Agent
+     * @see SocialForceModel
      */
     void affect(InteractiveEntity affectedEntity);
+
     /**
-     * get the class of Shape
+     * get the Shape of this entity.
      * line,square,circle etc.
      */
     Shape getShape();
+
+    /**
+     * get the total mass of the entity.
+     * usually the mass is all on the shape's reference point.
+     * @return the mass.
+     */
     double getMass();
+
+    /**
+     * get the social force model the entity is using.
+     * @return the model.
+     */
     SocialForceModel getModel();
+
+    /**
+     * set the social force model.
+     * @param model the model.
+     */
     void setModel(SocialForceModel model);
+
+    /**
+     * place this entity on a specific point.
+     * the shape will {@code moveTo} that point.
+     * @param point the target point.
+     */
     void placeOn(Point point);
 
 }
