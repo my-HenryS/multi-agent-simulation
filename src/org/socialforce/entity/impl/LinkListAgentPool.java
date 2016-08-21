1b2af9e7fd6176d06aae85889481c84850e1757f
Ledenel <0xCBA000@gmail.com>
2016-08-21 16:37:58 +0900

    add linklist pool for agent pool and entity pool.

---
+++ src/org/socialforce/entity/impl/LinkListAgentPool.java
@@ -0,0 +1,10 @@
+package org.socialforce.entity.impl;
+
+import org.socialforce.entity.Agent;
+import org.socialforce.entity.AgentPool;
+
+/**
+ * Created by Ledenel on 2016/8/21.
+ */
+public class LinkListAgentPool extends LinkListPool<Agent> implements AgentPool {
+}
---
+++ src/org/socialforce/entity/impl/LinkListEntityPool.java
@@ -0,0 +1,10 @@
+package org.socialforce.entity.impl;
+
+import org.socialforce.entity.EntityPool;
+import org.socialforce.entity.InteractiveEntity;
+
+/**
+ * Created by Ledenel on 2016/8/21.
+ */
+public class LinkListEntityPool extends LinkListPool<InteractiveEntity> implements EntityPool {
+}
