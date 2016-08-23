fa019c369d63735d2abcbdf3ad5ee60703b4e9f0
Ledenel <0xCBA000@gmail.com>
2016-08-24 01:07:15 +0900

    add generater and find for app.

---
+++ src/org/socialforce/app/SceneGenerator.java
@@ -0,0 +1,11 @@
+package org.socialforce.app;
+
+import org.socialforce.app.ParameterSet;
+import org.socialforce.app.Scene;
+
+/**
+ * Created by Ledenel on 2016/8/24.
+ */
+public interface SceneGenerator {
+    Iterable<Scene> generate(Scene template, ParameterSet params);
+}
