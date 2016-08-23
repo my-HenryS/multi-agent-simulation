91e8374fc5ef205fac3e0818c933448a1b6f6293
Ledenel <0xCBA000@gmail.com>
2016-08-23 12:24:25 +0900

    add right controls for the main panel.

--- src/org/socialforce/app/gui/SimulationPanelMain.java
+++ src/org/socialforce/app/gui/SimulationPanelMain.java
@@ -1,5 +1,7 @@
 package org.socialforce.app.gui;
 
+import com.sun.java.swing.plaf.windows.WindowsLookAndFeel;
+
 import javax.swing.*;
 
 /**
@@ -7,6 +9,19 @@ import javax.swing.*;
  */
 public class SimulationPanelMain {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        } catch (ClassNotFoundException e) {
+          //  e.printStackTrace();
+        } catch (InstantiationException e) {
+          //  e.printStackTrace();
+        } catch (IllegalAccessException e) {
+          //  e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+          //  e.printStackTrace();
+        } finally {
+            //UIManager.look
+        }
         JFrame frame = new JFrame("SimulationPanelMain");
         frame.setContentPane(new SimulationPanelMain().root);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -15,4 +30,10 @@ public class SimulationPanelMain {
     }
 
     private JPanel root;
+    private JTextField currentScnenTextField;
+    private JButton importFromFileButton;
+    private JComboBox agentPathFindingComboBox;
+    private JButton runButton;
+    private JButton pauseButton;
+    private JButton stopButton;
 }
