c8b5e93cba8addd4b8655945a0c51f3c8a345db3
Ledenel <0xCBA000@gmail.com>
2016-08-30 14:42:06 +0900

    fix typo.

--- src/org/socialforce/geom/ClipperShape.java
+++ src/org/socialforce/geom/ClipperShape.java
@@ -3,6 +3,6 @@ package org.socialforce.geom;
 /**
  * Created by Ledenel on 2016/8/30.
  */
-public interface CliperShape extends Shape {
+public interface ClipperShape extends Shape {
     Shape clip(ClipableShape shape);
 }
