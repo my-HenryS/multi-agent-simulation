1299b9d77461b4d4cbf19aa373d203eb8b771114
Ledenel <0xCBA000@gmail.com>
2016-08-31 00:09:21 +0900

    remove getter and setter for scene(unused).

--- src/org/socialforce/model/PathFinder.java
+++ src/org/socialforce/model/PathFinder.java
@@ -6,28 +6,19 @@ import org.socialforce.geom.Point;
 /**
  * a path finder for the agent in the scene.
  * used to generate path.
+ *
  * @author Ledenel
  * @see Path
  * Created by Ledenel on 2016/8/6.
  */
 public interface PathFinder {
-    /**
-     * get the scene where the path finder should search.
-     * @return the scene.
-     */
-    Scene getScene();
-
-    /**
-     * set the scene for the path finder to search.
-     * @param scene the scene.
-     */
-    void setScene(Scene scene);
 
     /**
      * generate a path from start point to end point.
+     *
      * @param agent the start point.
-     * @param goal the end point.
+     * @param goal  the end point.
      * @return the generated path.
      */
-    Path plan(Agent agent,Point goal);
+    Path plan(Agent agent, Point goal);
 }
