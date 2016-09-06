08e816f5d05dd37237ae68537d3d305da092d55f
Whatever <18910331889@189.cn>
2016-09-06 23:37:56 +0800

    初步导入了一些东西，整理了一下老分支，姑且先推着然后睡觉。

---
+++ src/org/socialforce/app/impl/preset/LoadSquareRoom.java
@@ -0,0 +1,33 @@
+package org.socialforce.app.impl.preset;
+
+import org.socialforce.app.ParameterSet;
+import org.socialforce.app.Scene;
+import org.socialforce.app.SceneLoader;
+
+import java.io.File;
+import java.io.InputStream;
+
+/**
+ * Created by Whatever on 2016/9/6.
+ */
+public class LoadSquareRoom implements SceneLoader {
+    @Override
+    public void setSource(InputStream stream) {
+
+    }
+
+    @Override
+    public void setSource(File file) {
+
+    }
+
+    @Override
+    public Scene readScene() {
+        return null;
+    }
+
+    @Override
+    public ParameterSet readParameterSet() {
+        return null;
+    }
+}
