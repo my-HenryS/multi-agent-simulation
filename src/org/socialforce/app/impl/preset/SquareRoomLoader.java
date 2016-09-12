79613b189a99da3f4044d4f6eb8298f42be833aa
Ledenel <0xCBA000@gmail.com>
2016-09-12 12:13:27 +0900

    add naming for walls.

--- src/org/socialforce/app/impl/preset/SquareRoomLoader.java
+++ src/org/socialforce/app/impl/preset/SquareRoomLoader.java
@@ -5,6 +5,7 @@ import org.socialforce.app.Scene;
 import org.socialforce.app.SceneLoader;
 import org.socialforce.app.SimpleScene;
 import org.socialforce.geom.impl.Box2D;
+import org.socialforce.geom.impl.Point2D;
 import org.socialforce.model.impl.Wall;
 
 import javax.naming.OperationNotSupportedException;
@@ -27,11 +28,18 @@ public class SquareRoomLoader implements SceneLoader {
 
     @Override
     public Scene readScene() {
-        Scene scene = new SimpleScene(new Box2D(-50,-50,100,100));
-        Wall [] walls = new Wall[4];
-        for(Wall wall : walls) {
-            //wall = new Wall();
+        Scene scene = new SimpleScene(new Box2D(-50, -50, 100, 100));
+        Wall[] walls = new Wall[]{
+                new Wall(new Box2D(new Point2D(0, 0), new Point2D(1, 16))),
+                new Wall(new Box2D(new Point2D(1, 15), new Point2D(25, 16))),
+                new Wall(new Box2D(new Point2D(25, 0), new Point2D(26, 16))),
+                new Wall(new Box2D(new Point2D(1, 0), new Point2D(25, 1)))
+        };
+        for (int i = 0;i<walls.length;i++) {
+            walls[i].setName("wall" + i);
+            scene.getStaticEntities().add(walls[i]);
         }
+
         return scene;
     }
 
