2f436d93bdb8bb0f726790f56756b27b54d1bfeb
Ledenel <0xCBA000@gmail.com>
2016-10-07 19:52:09 +0800

    change parameter signature.

--- src/org/socialforce/app/ParameterSet.java
+++ src/org/socialforce/app/ParameterSet.java
@@ -1,9 +1,11 @@
 package org.socialforce.app;
 
+import java.util.Collection;
 import java.util.Map;
 
 /**
  * Setting the parameter of Map
  */
-public interface ParameterSet extends Map<String,SceneParameter> {
+public interface ParameterSet extends Collection<SceneParameter> {
+    void findParameterByName(String name);
 }
