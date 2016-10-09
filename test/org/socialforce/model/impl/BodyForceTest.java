b833d5a888196cc23c75cd374f5285a22b298e55
sunjh <2994859298@qq.com>
2016-10-09 18:17:15 +0800

    add basic input

--- test/org/socialforce/model/impl/BodyForceTest.java
+++ test/org/socialforce/model/impl/BodyForceTest.java
@@ -1,6 +1,7 @@
 package org.socialforce.model.impl;
 
 import org.junit.Test;
+import org.socialforce.geom.impl.Circle2D;
 
 import static org.junit.Assert.*;
 
@@ -8,6 +9,8 @@ import static org.junit.Assert.*;
  * Created by sunjh1999 on 2016/10/9.
  */
 public class BodyForceTest {
+    BaseAgent baseAgent;
+    Circle2D circle2D;
     @Test
     public void hasForce() throws Exception {
 
