d56bfbd0e3d81fd73419aef8ff23b2032b907954
sunjh <2994859298@qq.com>
2016-10-09 18:08:06 +0800

    add tests for baseagent

---
+++ test/org/socialforce/model/impl/BaseAgentTest.java
@@ -0,0 +1,25 @@
+package org.socialforce.model.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.socialforce.geom.impl.Circle2D;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sunjh1999 on 2016/10/9.
+ */
+public class BaseAgentTest {
+    BaseAgent baseAgent;
+    Circle2D circle2D;
+    @Before
+    public void setUp() throws Exception{
+        baseAgent = new BaseAgent(circle2D);
+    }
+
+    @Test
+    public void getShape() throws Exception {
+        assertEquals(circle2D,baseAgent.getShape());
+    }
+
+}
