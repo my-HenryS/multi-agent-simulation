2608b58b0370a96ac844a3eea650e21ee336492e
sunjh <2994859298@qq.com>
2016-10-09 18:03:09 +0800

    initialize test file
    //todo fix lost tests

---
+++ test/org/socialforce/model/impl/BodyForceTest.java
@@ -0,0 +1,21 @@
+package org.socialforce.model.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sunjh1999 on 2016/10/9.
+ */
+public class BodyForceTest {
+    @Test
+    public void hasForce() throws Exception {
+
+    }
+
+    @Test
+    public void getForce() throws Exception {
+
+    }
+
+}
