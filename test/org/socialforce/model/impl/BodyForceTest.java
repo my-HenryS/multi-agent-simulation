20361318dc0a55a6d90fcbeda3f988242d14c347
sunjh <2994859298@qq.com>
2016-10-10 15:18:59 +0800

    find a null pointer error in Bodyforce; i ve test it yesterday with similar code. it shouldn't be a test-code-error

--- test/org/socialforce/model/impl/BodyForceTest.java
+++ test/org/socialforce/model/impl/BodyForceTest.java
@@ -1,7 +1,10 @@
 package org.socialforce.model.impl;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.socialforce.geom.impl.Circle2D;
+import org.socialforce.geom.impl.Point2D;
+import org.socialforce.model.impl.BodyForce;
 
 import static org.junit.Assert.*;
 
@@ -9,10 +12,29 @@ import static org.junit.Assert.*;
  * Created by sunjh1999 on 2016/10/9.
  */
 public class BodyForceTest {
-    BaseAgent baseAgent;
-    Circle2D circle2D;
+    BaseAgent baseAgent, targetAgent, targetAgent2;
+    Circle2D circle = new Circle2D(), circle2 = new Circle2D(), circle3 = new Circle2D();
+    Point2D initPoint, initPoint2;
+    BodyForce bodyForce = new BodyForce();
+    @Before
+    public void setUp() throws Exception{
+        initPoint = new Point2D(3,4);
+        initPoint2 = new Point2D(10,10);
+        circle.moveTo(initPoint);
+        circle.setRadius(5);
+        circle2.moveTo(initPoint2);
+        circle2.setRadius(5);
+        circle3.moveTo(initPoint2);
+        circle3.setRadius(1);
+        baseAgent = new BaseAgent(circle);
+        targetAgent = new BaseAgent(circle2);
+        targetAgent2 = new BaseAgent(circle3);
+
+    }
     @Test
     public void hasForce() throws Exception {
+        assertTrue(bodyForce.hasForce(baseAgent,targetAgent));                        //TODO 似乎有空指针错误 测试代码是照着昨天写的 应该不是测试的问题
+        assertFalse(bodyForce.hasForce(baseAgent,targetAgent2));
 
     }
 
