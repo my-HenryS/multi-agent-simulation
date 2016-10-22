45634db41cdab7b9a998c705a94a3004f4b9a136
Whatever <18910331889@189.cn>
2016-10-22 19:06:59 +0800

    release v0.1

---
+++ src/org/socialforce/app/Console/balabalaMain.java
@@ -0,0 +1,10 @@
+package org.socialforce.app.Console;
+
+/**
+ * Created by Whatever on 2016/10/22.
+ */
+public class balabalaMain  {
+    public static void main(String[] args) {
+        
+    }
+}
