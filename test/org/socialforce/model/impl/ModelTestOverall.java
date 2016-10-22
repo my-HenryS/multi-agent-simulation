e2bdd481b784c78f35e4e6b4fd13ac4e8f201139
sunjh <2994859298@qq.com>
2016-10-22 16:39:05 +0800

    add a test of only one agent

--- test/org/socialforce/model/impl/ModelTestOverall.java
+++ test/org/socialforce/model/impl/ModelTestOverall.java
@@ -19,7 +19,7 @@ import org.socialforce.model.InteractiveEntity;
 public class ModelTestOverall {
 
     SimpleScene scene = new SimpleScene(new Box2D(new Point2D(0, 0), new Point2D(20, 20)));
-    SVSR_AgentGenerator SVSR_agentGenerator = new SVSR_AgentGenerator(1,1,1,new Box2D(new Point2D(0, 0), new Point2D(9, 9)));
+    SVSR_AgentGenerator SVSR_agentGenerator = new SVSR_AgentGenerator(1,1,1,new Box2D(new Point2D(1, 1), new Point2D(2, 2)));
 
     @Before
     public void setUp() throws Exception {
@@ -30,12 +30,12 @@ public class ModelTestOverall {
     public void getAgent() throws Exception {
         AgentPool allAgents = scene.getAllAgents();
         for (Agent agent: allAgents){
-            agent.setPath(new StraightPath(agent.getShape().getReferencePoint(), new Point2D(2,2)));
+            agent.setPath(new StraightPath(agent.getShape().getReferencePoint(), new Point2D(4,4)));
             agent.setScene(scene);
         }
         scene.stepNext();
 
-        
+
     }
 
 
