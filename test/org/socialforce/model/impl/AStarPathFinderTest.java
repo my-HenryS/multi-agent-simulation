76a3f0736936860ff1ea68eadd913352ba425ea7
sunjh <2994859298@qq.com>
2016-10-26 00:37:46 +0800

    add tests for methods in astarpathfinder

--- test/org/socialforce/model/impl/AStarPathFinderTest.java
+++ test/org/socialforce/model/impl/AStarPathFinderTest.java
@@ -6,7 +6,9 @@ import org.junit.Test;
 import org.socialforce.app.Scene;
 import org.socialforce.app.impl.preset.SVSR_AgentGenerator;
 import org.socialforce.app.impl.preset.SquareRoomLoader;
+import org.socialforce.geom.Point;
 import org.socialforce.geom.impl.Box2D;
+import org.socialforce.geom.impl.Point2D;
 
 import static org.junit.Assert.*;
 
@@ -14,21 +16,19 @@ import static org.junit.Assert.*;
  * Created by sunjh1999 on 2016/10/23.
  */
 public class AStarPathFinderTest {
+    AStarPathFinder aStarPathFinder = new AStarPathFinder(new double[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}});
+    Point start_point;
+    Point goal;
     @Before
     public void setUp() throws Exception {
-        SquareRoomLoader loader = new SquareRoomLoader();
-        Scene scene = loader.readScene();
-        SVSR_AgentGenerator svsr_agentGenerator = new SVSR_AgentGenerator(1,1,1,new Box2D(3,3,2,2));
-        //BaseAgent new_agent = svsr_agentGenerator.agentGenerator.model.createAgent();
-
-
-        AStarPathFinder aStarPathFinder = new AStarPathFinder();
-        //aStarPathFinder.plan(scene,);
+        start_point = new Point2D(0,0);
+        goal = new Point2D(2,2);
     }
 
     @Test
     public void plan() throws Exception {
-
+        AStarPath path = aStarPathFinder.plan_for(start_point, goal);
+        System.out.println(path.toString());
     }
 
     @Test
