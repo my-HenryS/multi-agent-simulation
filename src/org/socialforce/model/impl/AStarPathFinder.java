41c14e2e662432d762455c5a80d60dbc3ee9f330
sunjh <2994859298@qq.com>
2016-10-25 17:31:47 +0800

    完成map的初始化

--- src/org/socialforce/model/impl/AStarPathFinder.java
+++ src/org/socialforce/model/impl/AStarPathFinder.java
@@ -19,43 +19,49 @@ import org.socialforce.model.PathFinder;
 public class AStarPathFinder implements PathFinder {
     static final int min_div = 1;
     double map[][];
+
     @Override
     public Path plan(Scene targetScene, Agent agent, Point goal) {
-        //AStarPath path = new AStarPath();
+        AStarPath path = new AStarPath();
         Point start_point = agent.getShape().getReferencePoint();
         EntityPool all_blocks = targetScene.getStaticEntities();
         for (InteractiveEntity entity : all_blocks) {
-            assert(!entity.getShape().contains(start_point));
+            assert (!entity.getShape().contains(start_point));
 
         }
         map = map_initiate(targetScene, agent, goal);
-        return null;
+        path = plan_for(agent);
+        return null;.
     }
 
-    public double [][] map_initiate(Scene targetScene, Agent agent, Point goal){
-        double x_range=targetScene.getBounds().getEndPoint().getX()-targetScene.getBounds().getStartPoint().getX(),
-                y_range=targetScene.getBounds().getEndPoint().getY()-targetScene.getBounds().getStartPoint().getY();
-        double[][] map = new double[(int)x_range/min_div][(int)y_range/min_div];
+    public double[][] map_initiate(Scene targetScene, Agent agent, Point goal) {
+        double x_range = targetScene.getBounds().getEndPoint().getX() - targetScene.getBounds().getStartPoint().getX(),
+                y_range = targetScene.getBounds().getEndPoint().getY() - targetScene.getBounds().getStartPoint().getY();
+        double[][] map = new double[(int) x_range / min_div][(int) y_range / min_div];
         EntityPool all_blocks = targetScene.getStaticEntities();
         Shape agent_shape = agent.getShape();
-        for(int i = 0; i<(int)x_range/min_div; i++){
-            for(int j = 0; j<(int)y_range/min_div; j++){
+        for (int i = 0; i < (int) x_range / min_div; i++) {
+            for (int j = 0; j < (int) y_range / min_div; j++) {
                 map[i][j] = 0;
                 for (InteractiveEntity entity : all_blocks) {
-                    agent_shape.moveTo(new Point2D(i,j));
+                    agent_shape.moveTo(new Point2D(i, j));
                     //assert( == entity.getShape().getClass());
-                    if(agent_shape.hits((Box2D)entity.getShape())){
+                    if (agent_shape.hits((Box2D) entity.getShape())) {
                         map[i][j] = 1;
-                    }
-                    else if(i<goal.getX() && goal.getX()<i+1 && j<goal.getY() && goal.getY()<=i+1){
+                    } else if (i < goal.getX() && goal.getX() < i + 1 && j < goal.getY() && goal.getY() <= i + 1) {
                         map[i][j] = 2;        //暂定2为目标点，目标点为goalx,y坐标的向下取整
-                    }
-                    else{
+                    } else {
                         map[i][j] = 0;        //暂定1为有障碍物占领
                     }
                 }
             }
         }
         return map;
+    }
+
+
+    public AStarPath plan_for(Agent agent) {
+        Point start_point = new Point2D((int) agent.getShape().getReferencePoint().getX(), (int) agent.getShape().getReferencePoint().getY());
+
     }
 }
