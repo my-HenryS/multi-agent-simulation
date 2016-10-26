0977bbe677b9f276ebf39cb04d6606ae5a0017ab
sunjh <2994859298@qq.com>
2016-10-26 16:54:29 +0800

    add method moveby to straightpath

--- src/org/socialforce/model/impl/StraightPath.java
+++ src/org/socialforce/model/impl/StraightPath.java
@@ -63,6 +63,13 @@ public class StraightPath implements Path {
         return reached >= goals.length;
     }
 
+    public Path moveBy(double x, double y){
+        for(int i =0; i<goals.length; i++){
+            goals[i].moveBy(x,y);
+        }
+        return this;
+    }
+
     public String toString(){
         String string = "路径为";
         for(int i =0; i<goals.length; i++){
