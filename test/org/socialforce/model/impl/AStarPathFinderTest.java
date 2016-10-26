1b8f64f6074e295080c6832e21c1ac98f2cdf43d
sunjh <2994859298@qq.com>
2016-10-26 14:11:25 +0800

    补充非硬编码初始pathfinder的测试

--- test/org/socialforce/model/impl/AStarPathFinderTest.java
+++ test/org/socialforce/model/impl/AStarPathFinderTest.java
@@ -8,7 +8,9 @@ import org.socialforce.app.impl.preset.SVSR_AgentGenerator;
 import org.socialforce.app.impl.preset.SquareRoomLoader;
 import org.socialforce.geom.Point;
 import org.socialforce.geom.impl.Box2D;
+import org.socialforce.geom.impl.Circle2D;
 import org.socialforce.geom.impl.Point2D;
+import org.socialforce.model.Path;
 
 import static org.junit.Assert.*;
 
@@ -19,21 +21,36 @@ public class AStarPathFinderTest {
     AStarPathFinder aStarPathFinder = new AStarPathFinder(new double[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}});
     Point start_point;
     Point goal;
+    Circle2D agent_shape = new Circle2D();
     @Before
     public void setUp() throws Exception {
         start_point = new Point2D(0,0);
         goal = new Point2D(2,2);
+        agent_shape.moveTo(new Point2D(4,4));
+        agent_shape.setRadius(0.484);
+        /*
+        start_point = new Point2D(0,0);
+        goal = new Point2D(0,0);
+        agent_shape.moveTo(new Point2D(20,20));               此种设定会出现行人规划路径避开障碍物的情况
+        agent_shape.setRadius(0.484);
+         */
     }
 
     @Test
-    public void plan() throws Exception {
+    public void plan_for() throws Exception {
         AStarPath path = aStarPathFinder.plan_for(start_point, goal);
         System.out.println(path.toString());
     }
 
     @Test
-    public void map_initiate() throws Exception {
+    public void plan() throws Exception {
+        SquareRoomLoader square = new SquareRoomLoader();
+        Scene scene = square.readScene();
+        Path path = aStarPathFinder.plan(scene, new BaseAgent(agent_shape) , goal);
+        System.out.println(path.toString());
 
     }
+
+
 
 }
