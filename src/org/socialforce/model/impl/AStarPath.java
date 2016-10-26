fa2b1766007ef9c72da0651342a9a8244cb3195d
sunjh <2994859298@qq.com>
2016-10-26 18:30:13 +0800

    make changes in astarpath

--- src/org/socialforce/model/impl/AStarPath.java
+++ src/org/socialforce/model/impl/AStarPath.java
@@ -44,7 +44,7 @@ public class AStarPath implements Path {
      */
     @Override
     public Point getCurrentGoal(Point current) {
-        while (reached < goals.length && goals[reached].epsilonEquals(current,10e-5)) {
+        while (reached < goals.length && goals[reached].epsilonEquals(current,10e-2)) {
             reached++;
         }
         if(done()) {
