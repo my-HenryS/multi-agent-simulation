9429cca78765f29dd7008f6dd4e466384be272c5
Whatever <18910331889@189.cn>
2016-11-01 21:04:17 +0800

    .

---
+++ test/org/socialforce/geom/impl/Rectangle2DTest.java
@@ -0,0 +1,74 @@
+package org.socialforce.geom.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Whatever on 2016/11/1.
+ */
+public class Rectangle2DTest {
+    Rectangle2D testRec,temp;
+    Point2D a,b,c,d;
+    @Before
+    public void setUp() throws Exception {
+        testRec = new Rectangle2D(new Point2D(3,3),4,6,Math.PI/4);
+    }
+
+    @Test
+    public void contains() throws Exception {
+
+    }
+
+    @Test
+    public void getDistance() throws Exception {
+
+    }
+
+    @Test
+    public void getReferencePoint() throws Exception {
+
+    }
+
+    @Test
+    public void getBounds() throws Exception {
+
+    }
+
+    @Test
+    public void hits() throws Exception {
+
+    }
+
+    @Test
+    public void moveTo() throws Exception {
+
+    }
+
+    @Test
+    public void cloneTest() throws Exception {
+
+    }
+
+    @Test
+    public void spin() throws Exception {
+
+    }
+
+    @Test
+    public void getAngle() throws Exception {
+
+    }
+
+    @Test
+    public void getScale() throws Exception {
+
+    }
+
+    @Test
+    public void equals() throws Exception {
+
+    }
+
+}
