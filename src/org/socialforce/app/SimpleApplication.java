6ed95738b276068b70031c0151164ffdc3d1caa4
sunjh <2994859298@qq.com>
2016-11-21 20:14:50 +0800

    去除硬编码的方式 并微调参数

--- src/org/socialforce/app/SimpleApplication.java
+++ src/org/socialforce/app/SimpleApplication.java
@@ -5,7 +5,9 @@ import org.socialforce.app.impl.preset.SVSR_Exit;
 import org.socialforce.app.impl.preset.SVSR_SafetyRegion;
 import org.socialforce.app.impl.preset.SquareRoomLoader;
 import org.socialforce.geom.impl.Box2D;
+import org.socialforce.geom.impl.Circle2D;
 import org.socialforce.geom.impl.Point2D;
+import org.socialforce.geom.impl.Semicircle2D;
 import org.socialforce.model.*;
 import org.socialforce.model.impl.*;
 
@@ -19,14 +21,14 @@ import java.util.stream.Stream;
  */
 public class SimpleApplication implements SocialForceApplication {
     public SimpleApplication() {
-        //singleScene = loader.readScene();
+        singleScene = loader.readScene();
         singleScene.setApplication(this);
         SetUp();
     }
 
     public void SetUp() {
         SVSR_Exit exit = new SVSR_Exit();
-        SVSR_AgentGenerator agentGenerator = new SVSR_AgentGenerator(2, 2, 1, new Box2D(3, 3, 22, 12));
+        SVSR_AgentGenerator agentGenerator = new SVSR_AgentGenerator(1, 1, 1, new Circle2D(new Point2D(13,6),7));
         SVSR_SafetyRegion safetyRegion = new SVSR_SafetyRegion();
         exit.setValue((new Box2D[]{new Box2D(-1, 5, 4, 6), new Box2D(10, -1, 6, 4), new Box2D(10, 14, 6, 4), new Box2D(24, 6, 4, 6)}));
         exit.apply(singleScene);
@@ -39,7 +41,7 @@ public class SimpleApplication implements SocialForceApplication {
         safetyRegion.apply(singleScene);
         safetyRegion.setValue(new SafetyRegion(new Box2D(28, 6, 4, 6)));
         safetyRegion.apply(singleScene);
-        singleScene.getStaticEntities().add(model.createStatic(new Box2D(0,0,1,7), SimpleSocialForceModel.STATIC_TYPE_WALL));
+        /*singleScene.getStaticEntities().add(model.createStatic(new Box2D(0,0,1,7), SimpleSocialForceModel.STATIC_TYPE_WALL));
         singleScene.getStaticEntities().add(model.createStatic(new Box2D(0,11,1,5), SimpleSocialForceModel.STATIC_TYPE_WALL));
         singleScene.getStaticEntities().add(model.createStatic(new Box2D(1,0,11,1), SimpleSocialForceModel.STATIC_TYPE_WALL));
         singleScene.getStaticEntities().add(model.createStatic(new Box2D(15,0,11,1), SimpleSocialForceModel.STATIC_TYPE_WALL));
@@ -47,6 +49,7 @@ public class SimpleApplication implements SocialForceApplication {
         singleScene.getStaticEntities().add(model.createStatic(new Box2D(15,15,10,1), SimpleSocialForceModel.STATIC_TYPE_WALL));
         singleScene.getStaticEntities().add(model.createStatic(new Box2D(25,1,1,7), SimpleSocialForceModel.STATIC_TYPE_WALL));
         singleScene.getStaticEntities().add(model.createStatic(new Box2D(25,12,1,4), SimpleSocialForceModel.STATIC_TYPE_WALL));
+        */
         //为解决穿墙的问题把墙每隔一米切分成多个墙
         /*
         for (InteractiveEntity entity : singleScene.getStaticEntities()){
