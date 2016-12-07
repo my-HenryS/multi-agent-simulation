be8bbda1d5317fcedf1f1e8da89472f93c0355c4
my-HenryS <2994859298@qq.com>
2016-12-07 14:14:56 +0800

    补充Point接口必要文档

--- src/org/socialforce/geom/Point.java
+++ src/org/socialforce/geom/Point.java
@@ -21,14 +21,19 @@ public interface Point extends Vector {
     double getY();
 
     /**
+     * 获取该点到另一个点的距离.
+     * @param other 需要计算距离的另一个点
+     * @return 距离.
+     */
+    double distanceTo(Point other);
+
+    /**
      * 获取该点到另一个点的距离矢量.
      * 可以是多维度的距离.
      * 方向指向目标点
      * @param other 需要计算距离的另一个点
      * @return 距离.
      */
-    double distanceTo(Point other);
-
     Vector directionTo(Point other);
 
     /**
@@ -37,9 +42,26 @@ public interface Point extends Vector {
      */
     Point clone();
 
+    /**
+     * 将该点移动到目标位置
+     * @param x x轴坐标
+     * @param y y轴坐标
+     * @return 移动后的点.
+     */
     Point moveTo(double x, double y);
 
+    /**
+     * 将该点根据偏移量移动到目标位置
+     * @param x x轴坐标偏差
+     * @param y y轴坐标偏差
+     * @return 移动后的点.
+     */
     Point moveBy(double x, double y);
 
+    /**
+     * 将该点根据乘数移动到目标位置
+     * @param s 乘数
+     * @return 移动后的点.
+     */
     Point scaleBy(double s);
 }
