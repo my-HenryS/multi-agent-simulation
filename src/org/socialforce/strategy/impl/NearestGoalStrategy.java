e06206878c2c063d9ce672cf9555ca87bb36a425
my-HenryS <2994859298@qq.com>
2016-12-21 15:02:22 +0800

    实现了nearestgoalstrategy,使得人可以根据goal距离的不同选择最近点

--- src/org/socialforce/strategy/impl/NearestGoalStrategy.java
+++ src/org/socialforce/strategy/impl/NearestGoalStrategy.java
@@ -2,10 +2,13 @@ package org.socialforce.strategy.impl;
 
 import org.socialforce.app.Scene;
 import org.socialforce.geom.Point;
+import org.socialforce.geom.impl.Point2D;
 import org.socialforce.model.Agent;
 import org.socialforce.strategy.Path;
 import org.socialforce.strategy.PathFinder;
 import org.socialforce.strategy.StaticStrategy;
+
+import java.util.Iterator;
 
 /**
  * Created by sunjh1999 on 2016/12/14.
@@ -15,23 +18,34 @@ public class NearestGoalStrategy implements StaticStrategy{
     Path designed_path;
     Point [] goals;
     Scene scene;
-    Agent agent;
     PathFinder pathFinder;
     double path_length = Double.POSITIVE_INFINITY;
 
-    public NearestGoalStrategy(Scene scene, Agent agent, PathFinder pathFinder, Point ... candidate_goals){
+    public NearestGoalStrategy(Scene scene, PathFinder pathFinder, Point ... candidate_goals){
         this.goals = candidate_goals;
         this.scene = scene;
-        this.agent = agent;
-        pathFinder.setParameters(scene);
         this.pathFinder = pathFinder;
     }
 
 
     @Override
-    public Point goal_decision() {
-        for(int i = 0; i < goals.length; i++){
-            if()
+    public void pathDecision(){
+        Agent agent;
+        for (Iterator iter = scene.getAllAgents().iterator(); iter.hasNext(); ) {
+            agent = (Agent) iter.next();
+            pathFinder.applyAgent(agent);
+            for (Point goal : goals) {
+                pathFinder.applyGoal(goal);
+                //设置最优path
+                Path path = pathFinder.plan_for();
+                double pathLength = path.length();
+                if (pathLength < path_length) {
+                    path_length = pathLength;
+                    designed_path = path;
+                }
+            }
+            agent.setPath(designed_path);
         }
+        pathFinder.postProcessing();
     }
 }
