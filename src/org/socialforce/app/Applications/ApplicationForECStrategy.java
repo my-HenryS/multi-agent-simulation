0422960d5efcd3b46b1c2cb8c20b0e8a2a8fc14d
sunjh <2994859298@qq.com>
2016-12-22 00:39:06 +0800

    场景部署

--- src/org/socialforce/app/Applications/ApplicationForECStrategy.java
+++ src/org/socialforce/app/Applications/ApplicationForECStrategy.java
@@ -73,7 +73,7 @@ public class ApplicationForECStrategy implements SocialForceApplication{
 
     public void SetUpParameter(){
         SimpleSceneParameter parameter = new SimpleSceneParameter();
-        parameter.addValue(new SVSR_RandomAgentGenerator(50,new Box2D(13,6,4.5,4.5)));
+        parameter.addValue(new SVSR_RandomAgentGenerator(400,new Box2D(4,4,26.5,14.5)));
         parameter.addValue(new SVSR_SafetyRegion(new Box2D(24,2,4,1)));
         parameter.addValue(new SVSR_SafetyRegion(new Box2D(33,12,1,4)));
         parameter.addValue(new SVSR_SafetyRegion(new Box2D(2,8,1,4)));
