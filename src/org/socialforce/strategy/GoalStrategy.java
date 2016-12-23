73ebdfa30a50b8718bc9b5cc6faf53e54b246c85
sunjh <2994859298@qq.com>
2016-12-23 08:23:20 +0800

    randomagentgenerator加入行人生成避开障碍物的判断（建议在agentgenerator中新开方法）

--- src/org/socialforce/strategy/GoalStrategy.java
+++ src/org/socialforce/strategy/GoalStrategy.java
@@ -1,10 +1,13 @@
 package org.socialforce.strategy;
 
 import org.socialforce.geom.Point;
+import org.socialforce.model.Agent;
 
 /**接受场景，寻路器和目标点集合
  * 实现为场景中的每一个Agent按既定策略规划目标点
  * 并调用Pathfinder生成路径，赋予Agent
+ * @see PathFinder
+ * @see Agent
  * Created by sunjh1999 on 2016/12/14.
  */
 public interface GoalStrategy {
