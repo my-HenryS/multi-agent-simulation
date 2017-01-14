0cde70f13c6c94634e9265939975ab1adafeaf36
my-HenryS <2994859298@qq.com>
2017-01-14 22:18:08 +0800

    strategy按重构后重新书写

--- src/org/socialforce/app/Applications/ApplicationForECStrategy.java
+++ src/org/socialforce/app/Applications/ApplicationForECStrategy.java
@@ -1,20 +1,16 @@
 package org.socialforce.app.Applications;
 
 import org.socialforce.app.*;
-import org.socialforce.app.impl.SimpleSceneParameter;
-import org.socialforce.app.impl.preset.*;
+import org.socialforce.app.impl.*;
 import org.socialforce.geom.impl.Box2D;
+import org.socialforce.geom.impl.Circle2D;
 import org.socialforce.geom.impl.Point2D;
-import org.socialforce.model.SocialForceModel;
-import org.socialforce.model.impl.SimpleSocialForceModel;
+import org.socialforce.model.impl.Wall;
 import org.socialforce.strategy.DynamicStrategy;
 import org.socialforce.strategy.PathFinder;
-import org.socialforce.strategy.StaticStrategy;
 import org.socialforce.strategy.impl.*;
 
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  * Created by Whatever on 2016/12/15.
@@ -22,9 +18,8 @@ import java.util.List;
 public class ApplicationForECStrategy extends ApplicationForECTest implements SocialForceApplication{
 
     public ApplicationForECStrategy(){
-        SetUpParameter();
         setUpScenes();
-        //setUpStrategy();
+        setUpStrategy();
     }
 
     /**
@@ -53,39 +48,29 @@ public class ApplicationForECStrategy extends ApplicationForECTest implements So
         }
     }
 
-    /**
-     * 需要根据parameter的map来生成一系列scene
-     * 目前就先随便生成一堆算了……
-     * 手动map
-     */
-    @Override
-    public void setUpScenes(){
-        for (Iterator<SceneParameter> iterator = parameters.iterator(); iterator.hasNext();)
-        {
-            SceneParameter parameter = iterator.next();
-            SceneLoader loader = new ECTestLoader2();
-            Scene scene = loader.readScene();
-            if (parameter instanceof SimpleSceneParameter){
-                while (true){
-                    SceneValue sceneValue =((SimpleSceneParameter) parameter).removeValue();
-                    if (sceneValue == null){break;}
-                    else sceneValue.apply(scene);
-                }
-            }
-            scene.setApplication(this);
-            scenes.addLast(scene);
-        }
-    }
 
-    @Override
-    public void SetUpParameter(){
-        SimpleSceneParameter parameter = new SimpleSceneParameter();
-        parameter.addValue(new SVSR_RandomAgentGenerator(400,new Box2D(4,4 ,27.5,15.5)));
-        parameter.addValue(new SVSR_SafetyRegion(new Box2D(24,2,4,1)));
-        parameter.addValue(new SVSR_SafetyRegion(new Box2D(33,12,1,4)));
-        parameter.addValue(new SVSR_SafetyRegion(new Box2D(2,8,1,4)));
-        parameter.addValue(new SVSR_SafetyRegion(new Box2D(12,21,4,1)));
-        parameters.addLast(parameter);
+    public void setUpScenes(){
+        SceneLoader loader = new StandardSceneLoader(new SimpleScene(new Box2D(-50, -50, 100, 100)),
+                new Wall[]{
+                        new Wall(new Box2D(new Point2D(4, 3), new Point2D(25, 4))),
+                        new Wall(new Box2D(new Point2D(26.75,3), new Point2D(32,4))),
+                        new Wall(new Box2D(new Point2D(32, 4), new Point2D(33, 13.5))),
+                        new Wall(new Box2D(new Point2D(32, 14.5), new Point2D(33, 20))),
+                        new Wall(new Box2D(new Point2D(3, 4), new Point2D(4, 9.5))),
+                        new Wall(new Box2D(new Point2D(3, 10.25), new Point2D(4,20))),
+                        new Wall(new Box2D(new Point2D(4, 20), new Point2D(13, 21))),//???
+                        new Wall(new Box2D(new Point2D(14.5,20), new Point2D(32, 21))),
+                        new Wall(new Circle2D(new Point2D(11.5,11),2)),
+                        new Wall(new Circle2D(new Point2D(26,11.5),2))
+                });
+        ValueSet values = new SimpleValueSet();
+        values.add(new SVSR_RandomAgentGenerator(400,new Box2D(4,4 ,27.5,15.5)));
+        values.add(new SVSR_SafetyRegion(new Box2D(24,2,4,1)));
+        values.add(new SVSR_SafetyRegion(new Box2D(33,12,1,4)));
+        values.add(new SVSR_SafetyRegion(new Box2D(2,8,1,4)));
+        values.add(new SVSR_SafetyRegion(new Box2D(12,21,4,1)));
+        loader.readValueSet(values);
+        scenes = loader.readScene(this);
     }
 
 }
