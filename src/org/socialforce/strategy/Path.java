2dae5532fd4dfaf58afdaaaddfd256c39831cfbb
my-HenryS <2994859298@qq.com>
2017-02-14 10:22:19 +0800

    Path接口调整

--- src/org/socialforce/strategy/Path.java
+++ src/org/socialforce/strategy/Path.java
@@ -3,6 +3,8 @@ package org.socialforce.strategy;
 import org.socialforce.geom.Point;
 import org.socialforce.model.Agent;
 
+import java.util.LinkedList;
+
 /**
  * 表示agent的路径。
  * 该接口的基本实现是获取一组目标点。
@@ -18,7 +20,9 @@ public interface Path {
      */
     Point getGoal();
 
+    Point getCurrentGoal();
 
+    LinkedList<Point> getGoals();
     /**
      *在当前位置获取下一个目标点。
      * @param current agent 所在的当前位置点。
