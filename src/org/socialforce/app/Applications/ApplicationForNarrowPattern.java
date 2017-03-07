0934ab782a00b2aa4d14ccfe90a21595be99f2b2
Whatever <18910331889@189.cn>
2017-03-07 15:19:20 +0800

    补充T1的变化宽度过道场景。

--- src/org/socialforce/app/Applications/ApplicationForNarrowPattern.java
+++ src/org/socialforce/app/Applications/ApplicationForNarrowPattern.java
@@ -8,6 +8,7 @@ import org.socialforce.geom.Shape;
 import org.socialforce.geom.impl.Box2D;
 import org.socialforce.geom.impl.Circle2D;
 import org.socialforce.geom.impl.Point2D;
+import org.socialforce.geom.impl.Rectangle2D;
 import org.socialforce.model.InteractiveEntity;
 import org.socialforce.model.impl.Monitor;
 import org.socialforce.scene.ParameterPool;
@@ -43,10 +44,6 @@ public class ApplicationForNarrowPattern extends SimpleApplication implements So
             while (!scene.getAllAgents().isEmpty()) {
                 scene.stepNext();
             }
-            for (Iterator<InteractiveEntity> iter = scene.getStaticEntities().selectClass(Monitor.class).iterator(); iter.hasNext(); ) {
-                Monitor monitor = (Monitor) iter.next();
-                System.out.println(monitor.sayVelocity());
-            }
         }
     }
 
@@ -58,6 +55,7 @@ public class ApplicationForNarrowPattern extends SimpleApplication implements So
         template = new Circle2D(new Point2D(0,0),0.486/2);
         DoorWidth = 1.36;
         setUpT1Scene();
+        setUpT1Scene2();
         setUpT2Scene();
         setUpPassageScene();
         setUpPassageScene2();
@@ -82,6 +80,22 @@ public class ApplicationForNarrowPattern extends SimpleApplication implements So
         }
     }
 
+    protected void setUpT1Scene2(){
+        InputStream is = this.getClass().getClassLoader().getResourceAsStream("T1.s");
+        Interpreter interpreter = new SimpleInterpreter();
+        interpreter.loadFrom(is);
+        SceneLoader loader = interpreter.setLoader();
+        ParameterPool parameters = new SimpleParameterPool();
+        parameters.addLast(genParameter(new SV_Exit(new Box2D[]{new Box2D(5-DoorWidth,-0.5,2*DoorWidth,2)})));
+        parameters.addLast(genParameter(new SV_RandomAgentGenerator(200,new Box2D(0,0,10,-10),template)));
+        parameters.addLast(genParameter(new SV_Wall(new Shape[]{new Rectangle2D(new Point2D(3,5),1,5,Math.PI/6),new Rectangle2D(new Point2D(7,5),1,5,-Math.PI/6)}),new SV_Wall(new Shape[]{new Rectangle2D(new Point2D(3,5),1,5,-Math.PI/6),new Rectangle2D(new Point2D(7,5),1,5,Math.PI/6),new Box2D(5-DoorWidth,0,DoorWidth/2,1),new Box2D(5+DoorWidth,0,-DoorWidth/2,1)}),new SV_Wall(new Shape[]{new Box2D(5-DoorWidth,0,DoorWidth/2,1),new Box2D(5+DoorWidth,0,-DoorWidth/2,1),new Rectangle2D(new Point2D(3,3),1,5,Math.PI/4),new Rectangle2D(new Point2D(7,3),1,5,-Math.PI/4),new Rectangle2D(new Point2D(3,6),1,5,-Math.PI/4),new Rectangle2D(new Point2D(7,6),1,5,Math.PI/4)})));
+        parameters.addLast(genParameter(new SV_SafetyRegion(new Box2D(1,10,8,1))));
+        loader.readParameterSet(parameters);
+        for (Scene s : loader.readScene(this)){
+            scenes.add(s);
+        }
+    }
+
     protected void setUpT2Scene(){
         InputStream is = this.getClass().getClassLoader().getResourceAsStream("T2.s");
         Interpreter interpreter = new SimpleInterpreter();
@@ -92,7 +106,7 @@ public class ApplicationForNarrowPattern extends SimpleApplication implements So
 
         parameters.addLast(genParameter(new SV_Exit(new Box2D[]{new Box2D(5-DoorWidth/2,-0.5,DoorWidth,2)}),new SV_Exit(new Box2D[]{new Box2D(10-DoorWidth,-0.5,DoorWidth,2)})));
 
-        parameters.addLast(genParameter(new SV_Wall(new Shape[]{}),new SV_Wall(new Shape[]{new Box2D(1,-10,1,9),new Box2D(3,-10,1,9),new Box2D(5,-10,1,9),new Box2D(7,-10,1,9),new Box2D(9,-10,1,9)})));
+        parameters.addLast(genParameter(new SV_Wall(new Shape[]{new Circle2D(new Point2D(3,-4),2),new Circle2D(new Point2D(7,-4),2)}),new SV_Wall(new Shape[]{}),new SV_Wall(new Shape[]{new Box2D(1,-10,1,9),new Box2D(3,-10,1,9),new Box2D(5,-10,1,9),new Box2D(7,-10,1,9),new Box2D(9,-10,1,9)})));
 
         parameters.addLast(genParameter(new SV_SafetyRegion(new Box2D(1,6,8,1))));
         loader.readParameterSet(parameters);
