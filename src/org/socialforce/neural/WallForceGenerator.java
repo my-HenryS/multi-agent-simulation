40e4474ebc602c3c0c1b59956d7d14d392344e6a
my-HenryS <2994859298@qq.com>
2017-03-27 17:16:43 +0800

    建立墙力类

---
+++ src/org/socialforce/neural/WallForceGenerator.java
@@ -0,0 +1,21 @@
+package org.socialforce.neural;
+
+/**
+ * Created by sunjh1999 on 2017/3/27.
+ */
+public class WallForceGenerator implements DataSetGenerator {
+    @Override
+    public void readFile(String directory) {
+
+    }
+
+    @Override
+    public void toFile(String directory) {
+
+    }
+
+    @Override
+    public double[][] toMatrix() {
+        return new double[0][];
+    }
+}
