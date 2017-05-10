58f966db38c9b6b1776f49571149402a012774de
my-HenryS <2994859298@qq.com>
2017-05-10 18:14:22 +0800

    test

--- src/org/socialforce/neural/impl/NeuralNetwork.java
+++ src/org/socialforce/neural/impl/NeuralNetwork.java
@@ -248,12 +248,12 @@ public class NeuralNetwork implements NeuralNetListener {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         NeuralNetwork nnetk = new NeuralNetwork();
-        nnetk.buildUp();
-        nnetk.run();
-        nnetk.saveNeuralNet("trainset4.net");
+        //nnetk.buildUp();
+        //nnetk.run();
+        //nnetk.saveNeuralNet("trainset4.net");
         nnetk.restoreNeuralNet("trainset4.net");
         nnetk.test(new double[][]{{
-                1,0,1,0,0,3,0,0,9,0,0,3
+                1,0.5, 0,0,0,0,0,0,0,0,0
         }});
     }
 }
